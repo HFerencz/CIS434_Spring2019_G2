eb1d254c7f751acf85d0143c51369cc8f4cfea35
Hayden Ferencz <ferenczhayden@gmail.com>
2019-03-18 00:03:23 -0400

    See Update Log for recent changes

---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,17 @@
+/*TicTacToe.java
+ * 
+ * GUI for playing a game of tic-tac-toe against either a human opponent or the computer. GUI
+ * designed using the Model-View-Controller architectural framework
+ * 
+ * @author: Hayden Ferencz
+ * February 18, 2019
+ * 
+ */
+public class TicTacToe {
+	public static void main(String[] args) {
+		View view = new View(900,900);
+		Model model = new Model();
+		Controller controller = new Controller(view, model);
+		controller.setActionListeners();
+	}
+}
